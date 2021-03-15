1fe76ffc73b07392ffe65e63ecd2074ece4b2fde
Noisyboy <63897832+noisyboy@users.noreply.github.com>
2021-03-15 23:14:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var source = "http://noisyboy.github.io/av.mp3";
 let audio = new Audio(source);
 image.addEventListener("mouseup", function() {
   alert("Don't dare to touch me");
-  if (confirm("Do you like Noisy ?")){
+  if (confirm(" *Do you think that Noisy is smart ?")){
   audio.play();
   }
   else {audio.pause();}
